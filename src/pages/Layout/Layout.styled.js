298a3d48823eef5afa7bbc654d8d92b975c404f0
YukhymykAlena <o.yukhymyk@gmail.com>
2023-02-24 16:38:37 +0200

    Update Layout.styled.js

--- src/pages/Layout/Layout.styled.js
+++ src/pages/Layout/Layout.styled.js
@@ -27,6 +27,7 @@ export const Nav = styled.nav`
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
 `;
 
+
 export const SNavLink = styled(NavLink)`
   font-weight: 600;
   font-size: 36px;
